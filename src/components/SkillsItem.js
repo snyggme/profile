cc364d5553e116fe1eb13db04446f4d04668d295
Бабахин Роман <babahin@ad.gksnab.ru>
2018-10-09 10:52:35 +0300

    responsive design

--- src/components/SkillsItem.js
+++ src/components/SkillsItem.js
@@ -4,7 +4,7 @@ export const SkillsItem = ({ logo }) => {
 	return (
 		<div className='skills-grid-block'>
 			<i className={logo.class}></i>
-			<div style={{textAlign: 'center', marginTop: '5px'}}>{logo.name}</div>
+			<div style={{textAlign: 'center', marginTop: '5px', fontSize: '1.2vw'}}>{logo.name}</div>
 		</div>
 	)
 }
