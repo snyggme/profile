4174c63f7c10541152f19ed99c53a2c6be79d2f1
Бабахин Роман <babahin@ad.gksnab.ru>
2018-10-10 17:42:15 +0300

    responsive design for small width

--- src/App.js
+++ src/App.js
@@ -14,33 +14,50 @@ class App extends Component {
             },
             nextPath: '',
             disableLinks: false,
-            showContent: false
+            showContent: false,
+            screenWidth: window.innerWidth
         }
     }
     render() {
-        let { newStyle, nextPath, disableLinks, showContent } = this.state
+        let { newStyle, nextPath, disableLinks, showContent, screenWidth } = this.state
 
-        if (nextPath === '' && getPathname(window.location.pathname) !== '/') {
+        if (nextPath === '' && getPathname(window.location.pathname) !== '/' && screenWidth > 663) {
             newStyle = {
                 animation: getAnimationName('/home', getPathname(window.location.pathname))
             }
+        } else if (screenWidth < 663) {
+            showContent = true
         }
+        
         return (
             <div>
                 <Header disableLinks={disableLinks} 
                         handleClick={(e) => {
+                            const screenWidth = window.innerWidth
                             const nextPath = getPathname(e.target.href)
                             const currentPath = getPathname(window.location.pathname)
 
                             if (nextPath !== currentPath) {
-                                this.setState({
-                                    newStyle: {
-                                        animation: getAnimationName(currentPath, nextPath),
-                                    },
-                                    nextPath: nextPath,
-                                    disableLinks: true,
-                                    showContent: false
-                                })  
+                                if(screenWidth < 663) {
+                                    document.getElementById('reverse-lines').click()
+
+                                    this.setState({
+                                        nextPath: nextPath,
+                                        disableLinks: false,
+                                        showContent: true,
+                                        screenWidth: screenWidth
+                                    })  
+                                } else {
+                                   this.setState({
+                                        newStyle: {
+                                            animation: getAnimationName(currentPath, nextPath)
+                                        },
+                                        nextPath: nextPath,
+                                        disableLinks: true,
+                                        showContent: false,
+                                        screenWidth: screenWidth
+                                    })   
+                                }
                             }        
                 }}/>
                 <div    className='main-background' style={newStyle}
