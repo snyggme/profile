132c9d5fd2366b5e2ddae6e30442c07be4529a79
snyggme <rcbabahin@gmail.com>
2018-11-10 12:33:12 +0300

    Updates

---
+++ precache-manifest.d6455b1ff6693d4a6c4f05098b096b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "72150887a8c494e6d64f",
+    "url": "/profile/static/js/main.72150887.chunk.js"
+  },
+  {
+    "revision": "f0f3d37c55fc9a6cb995",
+    "url": "/profile/static/js/1.f0f3d37c.chunk.js"
+  },
+  {
+    "revision": "72150887a8c494e6d64f",
+    "url": "/profile/static/css/main.21abcd81.chunk.css"
+  },
+  {
+    "revision": "35997b5cadfd6d8a47a9623210d1a82d",
+    "url": "/profile/index.html"
+  }
+];
